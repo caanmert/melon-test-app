b95b9e311429b9256dd95db38c8b73ce28419672
Can Mert <canmert.dev@gmail.com>
2023-05-29 10:46:34 +0300

    add: entry file for screens

---
+++ app/screens/index.ts
@@ -0,0 +1,5 @@
+import HomeScreen from "./home/home-screen";
+import LandingScreen from "./landing/landing-screen";
+import LocationFetchScreen from "./location-fetch/location-fetch-screen";
+
+export { HomeScreen, LandingScreen, LocationFetchScreen };
