29266f5bb33492d3ec48d76e332177ef6d4033e0
Can Mert <canmert.dev@gmail.com>
2023-05-29 10:44:50 +0300

    add: weather code icons

---
+++ app/constants/weather-code-icons.ts
@@ -0,0 +1,36 @@
+import sunImage from "../../assets/sun.png";
+import partlyCloudImage from "../../assets/partlycloudy.png";
+import mistImage from "../../assets/mist.png";
+import moderateRainImage from "../../assets/moderaterain.png";
+import heavyRainImage from "../../assets/heavyrain.png";
+
+export const weatherCodeIcons = {
+  0: sunImage,
+  1: partlyCloudImage,
+  2: partlyCloudImage,
+  3: partlyCloudImage,
+  45: mistImage,
+  48: mistImage,
+  51: mistImage,
+  53: mistImage,
+  55: mistImage,
+  56: mistImage,
+  57: mistImage,
+  61: moderateRainImage,
+  63: moderateRainImage,
+  65: moderateRainImage,
+  66: heavyRainImage,
+  67: heavyRainImage,
+  71: heavyRainImage,
+  73: heavyRainImage,
+  75: heavyRainImage,
+  77: heavyRainImage,
+  80: heavyRainImage,
+  81: heavyRainImage,
+  82: heavyRainImage,
+  85: heavyRainImage,
+  86: heavyRainImage,
+  95: heavyRainImage,
+  96: heavyRainImage,
+  99: heavyRainImage,
+};
